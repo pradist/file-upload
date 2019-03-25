315f091f3eb650b41b4d2565aba76488f7f83d4c
pradist <pradist.ke@gmail.com>
2019-03-25 23:51:16 +0700

    Implement file upload rest API.

---
+++ README.md
@@ -0,0 +1,25 @@
+# File Upload rest API.
+
+**Tutorial**: Uploading files with Spring Boot using gradle
+
+## Steps to Setup
+
+**1. Clone the repository** 
+
+```bash
+git clone https://github.com/pradist/file-upload.git
+```
+
+**2. Run the app using maven**
+
+```bash
+cd file-upload
+gradle bootRun
+```
+
+You may also package the application in the form of a jar and then run the jar file like so -
+
+```bash
+gradle clean build
+java -jar build/libs/file-uploads.jar
+```
--- src/main/java/com/demo/filedemo/FileDemoApplication.java
+++ src/main/java/com/demo/filedemo/FileDemoApplication.java
@@ -1,9 +1,14 @@
 package com.demo.filedemo;
 
+import com.demo.filedemo.property.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+		FileStorageProperties.class
+})
 public class FileDemoApplication {
 
 	public static void main(String[] args) {
